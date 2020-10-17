ae51fad127a7f40618e182053e3f59f622769438
Christoffer Pedersen <chriskpedersen@hotmail.com>
2020-10-17 23:10:38 +0200

    Update UnitTest1.cs

--- src/UnitTestProject/UnitTest1.cs
+++ src/UnitTestProject/UnitTest1.cs
@@ -6,10 +6,5 @@ namespace UnitTestProject
   [TestClass]
   public class UnitTest1
   {
-    [TestMethod]
-    public void FtpFileDialog_ConnectionDetails_UseFtpPathURL()
-    {
-      var ftpCred = new FtpFileDialog.ConnectionDetails("ftp://AdvosysDK2:9vgnJRK2fGF8@test.acubizems.com/");
-    }
   }
 }
